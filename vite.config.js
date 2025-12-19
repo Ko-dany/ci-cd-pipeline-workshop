e20a05ee02d8153338d3c5be2a506a770bb03748
Ko-dany <iamgo910@gmail.com>
2025-12-19 14:35:33 -0500

    Change subtitle

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/ko-dany.github.io-ci-cd-pipeline-workshop/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
